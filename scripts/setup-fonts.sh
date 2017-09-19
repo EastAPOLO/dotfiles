b4df4acd892076d1ef094a0ababf9d219fe96c7e
EastAPOLO <eastapolo@hotmail.com>
2017-09-18 23:36:40 -0500

    Update dotfiles

--- scripts/setup-fonts.sh
+++ scripts/setup-fonts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+LOCAL_FONTCONFIG_DIR=~/.config/fontconfig 
+
 sudo pacman -S ttf-dejavu ttf-liberation noto-fonts
+yaourt -S ttf-ms-fonts 
 
 echo "Symlinking fonts ..."
 
@@ -8,5 +11,29 @@ sudo ln -sf /etc/fonts/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d
 sudo ln -sf /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 sudo ln -sf /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 sudo ln -sf /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+
+mkdir ${LOCAL_FONTCONFIG_DIR}
+
+cat > "${LOCAL_FONTCONFIG_DIR}/fonts.conf" << "EOF"
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <match>
+        <edit mode="prepend" name="family"><string>Noto Sans</string></edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family"><string>serif</string></test>
+        <edit name="family" mode="assign" binding="same"><string>Noto Serif</string></edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family"><string>sans-serif</string></test>
+        <edit name="family" mode="assign" binding="same"><string>Noto Sans</string></edit>
+    </match>
+    <match target="pattern">
+        <test qual="any" name="family"><string>monospace</string></test>
+        <edit name="family" mode="assign" binding="same"><string>Noto Mono</string></edit>
+    </match>
+</fontconfig>
+EOF
 
 
