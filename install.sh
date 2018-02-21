226633961c40ac7a87b6e3a8400ce1f9838c0a90
EastAPOLO <eastapolo@hotmail.com>
2018-02-20 19:01:04 -0600

    Add install script for dotfiles

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Copy config folders and files to home dir
+if [ -d "$HOME/.config" ]; then
+    cp -rv ".config/." "$HOME/.config/"
+else
+    mkdir -v "$HOME/.config"
+    cp -rv ".config/." "$HOME/.config/"
+fi
+
+# Copy bash config files to home dir
+cp -v ".bashrc" "$HOME/"
+cp -v ".bash_profile" "$HOME/"
+cp -v ".bash_logout" "$HOME/"
+
+# Copy vimrc to home dir
+cp -v ".vimrc" "$HOME/"
+
+# Copy xinitrc to home dir
+cp -v ".xinitrc" "$HOME/"
+
+# Copy Xresources to home dir
+cp -v ".Xresources" "$HOME/"
