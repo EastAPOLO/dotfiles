86dd4d55d150ef97ae3b71eb86b1d2b3524fd658
EastAPOLO <eastapolo@hotmail.com>
2018-02-26 18:00:45 -0600

    Update vim-plug install script

--- scripts/other/vim-plug-install.sh
+++ scripts/other/vim-plug-install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+
+if [ -f "$HOME/.vimrc" ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [ -f "$HOME/.config/nvim/init.vim" ]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
