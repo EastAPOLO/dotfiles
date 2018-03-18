01f18847477759506683372f5eb1925fd358fa1d
EastAPOLO <eastapolo@hotmail.com>
2018-03-18 02:14:06 -0500

    Remove unused scripts and update install.sh

--- install.sh
+++ install.sh
@@ -25,12 +25,62 @@ cp -v ".Xresources" "$HOME/"
 # Copy etc config files to /etc
 sudo cp -rv ".config/etc/"* "/etc/"
 
-if [ -f "/usr/bin/apt" ]; then
+# Install my favorite programs if apt is detected on the system
+if [ -x "$(command -v apt)" ]; then
     sudo apt update && sudo apt upgrade
-    sudo apt install htop zathura libreoffice synaptic git hexchat fonts-noto
+    sudo apt purge nano vi vim
+    sudo apt install htop zathura libreoffice synaptic git hexchat fonts-noto neovim
 fi
 
-if [ -f "/usr/bin/pacman" ]; then
+# Install my favorite programs if pacman is detected on the system
+if [ -x "$(command -v pacman)" ]; then
     sudo pacman -Syu
-    sudo pacman -S htop libreoffice i3-wm termite zathura-pdf-mupdf hexchat noto-fonts
+    sudo pacman -Rsn nano vi vim
+    sudo pacman -S htop libreoffice i3-wm termite zathura-pdf-mupdf hexchat noto-fonts neovim ttf-dejavu ttf-ubuntu-font-family
+fi
+
+# Set sane font rendering defaults
+LOCAL_FONTCONFIG_DIR="$HOME/.config/fontconfig" 
+
+echo "Symlinking fonts ..."
+
+sudo ln -sfv "/etc/fonts/conf.avail/11-lcdfilter-default.conf" "/etc/fonts/conf.d"
+sudo ln -sfv "/etc/fonts/conf.avail/10-sub-pixel-rgb.conf" "/etc/fonts/conf.d"
+sudo ln -sfv "/etc/fonts/conf.avail/10-hinting-slight.conf" "/etc/fonts/conf.d"
+sudo ln -sfv "/etc/fonts/conf.avail/70-no-bitmaps.conf" "/etc/fonts/conf.d"
+
+mkdir -v ${LOCAL_FONTCONFIG_DIR}
+
+cat > "${LOCAL_FONTCONFIG_DIR}/fonts.conf" << "EOF"
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+    <match target="font">
+        <edit name="autohint" mode="assign">
+            <bool>true</bool>
+        </edit>
+        <edit name="hinting" mode="assign">
+            <bool>true</bool>
+        </edit>
+        <edit mode="assign" name="hintstyle">
+            <const>hintslight</const>
+        </edit>
+        <edit mode="assign" name="lcdfilter">
+            <const>lcddefault</const>
+        </edit>
+    </match>
+</fontconfig>
+EOF
+
+echo "Font configuration complete."
+
+# Setup vim-plug for neovim and vim
+if [ -f "$HOME/.vimrc" ] && [ -x "$(command -v vim)" ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+if [ -f "$HOME/.config/nvim/init.vim" ] && [ -x "$(command -v nvim)" ]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
