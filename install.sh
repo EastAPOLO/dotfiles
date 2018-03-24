e2289aa4e60743a1e9c76435860e151e71c559b5
EastAPOLO <eastapolo@hotmail.com>
2018-03-24 00:47:59 -0500

    Update install.sh and add fontconfig

--- install.sh
+++ install.sh
@@ -40,37 +40,12 @@ if [ -x "$(command -v pacman)" ]; then
 fi
 
 # Set sane font rendering defaults
-LOCAL_FONTCONFIG_DIR="$HOME/.config/fontconfig" 
-
 echo "Symlinking fonts ..."
 
 sudo ln -sfv "/etc/fonts/conf.avail/11-lcdfilter-default.conf" "/etc/fonts/conf.d"
 sudo ln -sfv "/etc/fonts/conf.avail/10-sub-pixel-rgb.conf" "/etc/fonts/conf.d"
 sudo ln -sfv "/etc/fonts/conf.avail/10-hinting-slight.conf" "/etc/fonts/conf.d"
 sudo ln -sfv "/etc/fonts/conf.avail/70-no-bitmaps.conf" "/etc/fonts/conf.d"
-
-mkdir -v ${LOCAL_FONTCONFIG_DIR}
-
-cat > "${LOCAL_FONTCONFIG_DIR}/fonts.conf" << "EOF"
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-    <match target="font">
-        <edit name="autohint" mode="assign">
-            <bool>true</bool>
-        </edit>
-        <edit name="hinting" mode="assign">
-            <bool>true</bool>
-        </edit>
-        <edit mode="assign" name="hintstyle">
-            <const>hintslight</const>
-        </edit>
-        <edit mode="assign" name="lcdfilter">
-            <const>lcddefault</const>
-        </edit>
-    </match>
-</fontconfig>
-EOF
 
 echo "Font configuration complete."
 
