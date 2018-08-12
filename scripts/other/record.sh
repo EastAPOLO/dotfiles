e2f96c1b21f9307a1baf0609f7bc67b19e3351c5
EastAPOLO <eastapolo@hotmail.com>
2018-08-12 18:21:01 -0500

    Update record script

--- scripts/other/record.sh
+++ scripts/other/record.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# If recording is in process, cancel it to start transcoding
+running=$(ps ax | grep ffmpeg | wc -l)
+if [[ $running != 1 ]]; then
+    pkill ffmpeg;
+    exit 0;
+fi
+
 ffmpeg -video_size 1920x1080 -framerate 60 -f x11grab -i :0.0+0,0 -f alsa -ac 2 -i pulse -acodec libvorbis "$@".mkv
